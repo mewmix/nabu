aca5bc45e98906027cdd7142abae13c8a7612f4e
Alex Klein <alex@facings.io>
2025-08-17 18:44:58 -0700

    fix import

--- app/src/main/java/com/example/nabu/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/nabu/screens/SettingsScreen.kt
@@ -2,6 +2,7 @@ package com.example.nabu.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExposedDropdownMenuBox
