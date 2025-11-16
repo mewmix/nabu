614871f4a1769f3e620b16014ca43ab7135311bb
mewmix <him@Alexanders-MacBook-Pro.local>
2025-11-16 09:40:01 -0800

    Bump version to 0.4.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "0.4.6"
+        versionName = "0.4.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
