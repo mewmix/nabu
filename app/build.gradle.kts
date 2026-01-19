d07793b8f7065c19c6ff5a8ed05afe804cde89e2
mewmix <him@Alexanders-MacBook-Pro.local>
2026-01-18 16:49:29 -0800

    Bump version to 0.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.mewmix.nabu"
         minSdk = 29
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.5.0"
+        versionCode = 5
+        versionName = "0.5.1"
 
         val gitCommitHashProvider = providers.exec {
             commandLine("git", "rev-parse", "--short=7", "HEAD")
